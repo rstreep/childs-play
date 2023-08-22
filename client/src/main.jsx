07afcc0309bc9c2e4bbcc420c79baa060f3ded0e
rstreep <127055958+rstreep@users.noreply.github.com>
2023-08-22 18:43:45 -0400

    added basic file structure in react for pages/main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,49 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Homepage from "./pages/Homepage.jsx";
+import Login from "./pages/Login.jsx";
+import AnimalGame from "./pages/AnimalGame.jsx";
+import ColorGame from "./pages/ColorGame.jsx";
+import SpellingGame from "./pages/SpellingGame.jsx";
+
+const routes = [
+  {
+    path:"/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/animalgame",
+        element: <AnimalGame />
+      },
+      {
+        path: "/colorgame",
+        element: <ColorGame />
+      },
+      {
+        path: "/spellinggame",
+        element: <SpellingGame />
+      }
+    ]
+  }
+]
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);
