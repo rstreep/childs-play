0431bcf25f950281263bc9fc61b0675d1d53b6fc
mayamouse <cooperpontea@gmail.com>
2023-08-22 21:14:49 -0400

    setting up structure for game

--- client/src/pages/SpellingGame.jsx
+++ client/src/pages/SpellingGame.jsx
@@ -1,3 +1,35 @@
+import React, {useState, useEffect } from 'react';
+
+//list words, answers, images, and sounds in an array
+const wordsAndAnswers = [
+    { word:'CAT', answer: 'C', image: '', sound: '' },
+    { word:'BED', answer: 'D', image: '', sound: '' },
+    { word:'BIRD', answer: 'R', image: '', sound: '' },
+    { word:'SNOW', answer: 'N', image: '', sound: '' },
+    { word:'DOG', answer: 'G', image: '', sound: '' },
+    { word:'SUN', answer: 'U', image: '', sound: '' },
+    { word:'TOY', answer: 'O', image: '', sound: '' },
+    { word:'HAT', answer: 'H', image: '', sound: '' },
+    { word:'MOON', answer: 'O', image: '', sound: '' },
+    { word:'FISH', answer: 'S', image: '', sound: '' },
+    { word:'TREE', answer: 'E', image: '', sound: '' },
+    { word:'BALL', answer: 'L', image: '', sound: '' },
+    { word:'MOP', answer: 'P', image: '', sound: '' },
+    { word:'PIG', answer: 'I', image: '', sound: '' },
+    { word:'BOOK', answer: 'B', image: '', sound: '' },
+    { word:'STAR', answer: 'S', image: '', sound: '' },
+    { word:'COW', answer: 'W', image: '', sound: '' },
+    { word:'BOAT', answer: 'O', image: '', sound: '' },
+];
+
+const generateOptions = () => {
+
+};
+
+const SpellingGame = () =>{
+
+};
+
 export default function SpellingGame() {
 
 };
