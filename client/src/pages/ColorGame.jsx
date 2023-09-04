c6ff1aba595906fd3e622e8e78ac1ce242768c34
Alice Li <aliceli083@gmail.com>
2023-09-03 23:45:16 -0700

    css issues

--- client/src/pages/ColorGame.jsx
+++ client/src/pages/ColorGame.jsx
@@ -1,8 +1,9 @@
 import Colorgame from '../components/color_components';
-
+import food_wallpaper from '../assets/images/colorgame/food_wallpaper.jpg'
 const ColorGame = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center ">
+
       <Colorgame/>
     </div>
   );
